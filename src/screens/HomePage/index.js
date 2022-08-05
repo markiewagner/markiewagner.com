4991abcf9c7f784f1ea063fe701d151a131dce46
Markie Wagner <markiewagner@Markies-MacBook-Pro.local>
2022-08-04 21:02:37 -0700

    Updated language

--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -8,7 +8,7 @@ export const HomePage = () => {
       <div style={homeStyles.body}>
         {/* Body text */}
         <p>I'm the founder of Delphi Labs</p>
-        <p>After we're gone, only a handful of companies will remain in the history books as essential in pushing forward a significant economic, cultural, or technological movement. Delphi allies with founders embarking on grand quests to enter the canon.</p>
+        <p>After we're gone, only a handful of companies will remain in the history books as essential in pushing forward a significant economic, cultural, or technological movement. Delphi allies with founders on a quest to enter this canon.</p>
         <p>We partner with companies in a hands-on way to build and go to market with aggressively ambitious applications of new technology.</p>
         <p>Before Delphi, I was a researcher at Stanford and Waymo.</p>
         <p><i>Interests include cult building infrastructure, new nations, psychographics, zk-proof applications, and artificial intelligence (particularly reinforcement learning and graph neural nets).</i></p>
