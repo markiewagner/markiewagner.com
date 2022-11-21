54378f662e23763d9bd6c55b74f950c4762799cf
Markie Wagner <markiewagner@Markies-MacBook-Pro.local>
2022-11-21 13:19:46 -0800

    Copy update

--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -7,12 +7,12 @@ export const HomePage = () => {
       <h1 style={homeStyles.title}>Markie Wagner</h1>
       <div style={homeStyles.body}>
         {/* Body text */}
-        <p>I'm the is the founder of Delphi Labs, a frontier tech research and advisory firm with a focus on applied AI. </p>
-        <p>We partner with companies in a hands-on way to build and go to market with aggressively ambitious applications of new technology.</p>
+        <p>I'm the founder of Delphi Labs, a frontier tech research and advisory firm with a focus on applied AI. </p>
+        <p>We partner with companies to build and go to market with aggressively ambitious applications of new technology.</p>
 
         <p>Our goal is play a mission-critical role in the creation of institutions that matter in the human story.</p>
         <p>Before Delphi, I was a researcher at Stanford and Waymo.</p>
-        <p><i>Interests include cult building infrastructure, new nations, psychographics, zk-proof applications, and artificial intelligence (particularly reinforcement learning and graph neural nets).</i></p>
+        <p><i>Interests include cult building, new nations, psychographics, zk-proof applications, automating everything, and artificial intelligence (particularly reinforcement learning and graph neural nets).</i></p>
         {/* Links */}
         <br />
         <a style={homeStyles.links} href={"https://twitter.com/markiewagner"}>
